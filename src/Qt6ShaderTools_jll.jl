6de6ebad04f217b05dafca896db6bd285049b443
jlbuild <juliabuildbot@gmail.com>
2026-02-21 09:43:15 +0000

    Qt6ShaderTools_jll build 6.10.2+0

--- src/Qt6ShaderTools_jll.jl
+++ src/Qt6ShaderTools_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6ShaderTools")
-JLLWrappers.@generate_main_file("Qt6ShaderTools", UUID("ce943373-25bb-56aa-8eca-768745ed7b5a"))
+JLLWrappers.@generate_main_file("Qt6ShaderTools", Base.UUID("ce943373-25bb-56aa-8eca-768745ed7b5a"))
 end  # module Qt6ShaderTools_jll
